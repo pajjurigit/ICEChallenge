cf3b01bb60c5f54a60a5c9fe3eee7de38f4300d1
pajjurigit <pajjuri@gmail.com>
2019-05-05 20:44:52 -0400

    ICE Challenges

---
+++ IceChallenge/Anagrams.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceChallenge
+{
+    public class Anagrams
+    {
+        private Dictionary<string, List<string>> anagrams = new Dictionary<string, List<string>>();
+        public void ListAllAnagrams(string sentence)
+        {
+            var words = sentence.Split(' ');
+
+            for(int i=0; i< words.Count(); i++)
+            {
+                string word = words[i];
+                char[] wchars = word.ToCharArray();
+                Array.Sort(wchars);
+                string sortedWord = new string(wchars);
+
+                if(anagrams.ContainsKey(sortedWord))
+                {
+                    var list = anagrams[sortedWord];
+                    list.Add(word);
+                }
+                else
+                {
+                    anagrams.Add(sortedWord, new List<string>() { word });
+                }
+
+            }
+            WriteAllAnagrams();
+        }
+
+        public void WriteAllAnagrams()
+        {
+            foreach (var entry in anagrams)
+            {
+                Console.Write("[ {0}: ", entry.Key);
+                foreach (string word in entry.Value)
+                {
+                    Console.Write(word + " ");
+                }
+                Console.Write("]");
+                Console.WriteLine();
+            }
+        }
+        
+    }
+}
---
+++ IceChallenge/CusipPrices.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace IceChallenge
+{
+    public class CusipPrices
+    {
+        private Dictionary<string, string> dict = new Dictionary<string, string>();
+
+        public bool ProcessCusipFiles(string folderPath)
+        {
+            //folderPath=@"Data\Names.txt";
+
+            if (string.IsNullOrEmpty(folderPath)) return false;
+
+            string lastCusip = "";
+            foreach (string file in Directory.EnumerateFiles(folderPath, "*.txt"))
+            {
+                lastCusip = ProcessCusipPriceFile( file, lastCusip);
+            }
+
+            WriteResultsToATextFile();
+
+            return true;
+        }
+
+        public bool WriteResultsToATextFile()
+        {
+            using (StreamWriter file = new StreamWriter("LatestCusipPrices.txt"))
+            {
+                foreach (var entry in dict)
+                    file.WriteLine("[{0} {1}]", entry.Key, entry.Value);
+            }
+
+            return true;
+
+        }
+
+        public string ProcessCusipPriceFile(string filePath, string lastFileCusip="")
+        {
+
+            if (string.IsNullOrEmpty(filePath)) return "";
+            string currCusip = lastFileCusip;
+
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                //String row = "", current = "", previous = "", price = "";
+                string row = "";
+                Regex r = new Regex("^[a-zA-Z0-9]*$");
+
+                 //to keep track of cusips, between the files...
+                //bool skipInvalid = false;
+
+                while((row=sr.ReadLine()) != null)
+                {
+                    if(r.IsMatch(row) && row.Length==8)
+                    {
+                        currCusip = row;
+                        //add it to Hashtable
+                        if(!dict.ContainsKey(row)) { dict.Add(row, ""); }
+                        
+                    }
+                    else
+                    {
+                      //  if (skipInvalid) continue;
+                        double result;
+                        if(Double.TryParse(row, out result)) {
+                            dict[currCusip] = row; //this gets updated until next cusip is read from the file...
+                        }
+                        //else { skipInvalid = true; }
+                    }
+
+                }
+            }
+
+                return currCusip;
+        }
+
+    }
+}
